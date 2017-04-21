afc3488114ce96071165fa7be83e3d96e4aba607
Andrés Gonzalez <smiith17@gmail.com>
2017-04-21 14:48:03 -0400

    Finalizando el modulo de pacientes

--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -40,10 +40,10 @@
                         <a href="/pacientes/{{$paciente->id}}/edit" class="btn btn-success" data-toggle='tooltip' title='Editar' target="_blank">
                             <i class='glyphicon glyphicon-edit'></i>
                         </a>
-                        {!! Form::open(['route' => 'pacientes.index', 'method' => 'delete', 'role' => 'search']) !!}
-                        <a href="/pacientes/{{$paciente->id}}/edit" class="btn btn-danger" data-toggle='tooltip' title='Eliminar' target="_blank">
+                        {!! Form::open(['route' => ['pacientes.destroy', $paciente->id], 'method' => 'delete']) !!}
+                        <button type="submit" class="btn btn-danger" data-toggle='tooltip' title='Eliminar' target="_blank">
                             <i class='glyphicon glyphicon-remove'></i>
-                        </a>
+                        </button>
                         {!! Form::close() !!}
                     </td>
                 </tr>
