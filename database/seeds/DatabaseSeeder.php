a7ea31a5ec43bd8ce0c4b43b993fe9b21da30ad5
Angel Moran <angel.javier01@gmail.com>
2017-05-03 15:12:11 -0400

    modificacion de dabaseseeder para que me corriera el seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use App\Paciente;
 use App\Aseguradora;
 use App\Empresa;
 use App\ordenservicios;
+use App\Diagnosticos;
 
 
 class DatabaseSeeder extends Seeder
@@ -20,23 +21,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //Model::unreguard();
-        /*Cie10::truncate();
+       // Model::unreguard();
+       Cie10::truncate();
         $this->call('Cie10Seeder');
         Medicamentos::truncate();
         $this->call('MedicamentosSeeder');
         Servicios::truncate();
-        $this->call('ServiciosSeeder');*/
+        $this->call('ServiciosSeeder');
 
         Aseguradora::truncate();
         $this->call('AseguradoraSeeder');
         Paciente::truncate();
         $this->call('PacientesSeeder');
+        Empresa::truncate();
         $this->call('EmpresaSeeder');
+        Diagnosticos::truncate();
         $this->call('DiagnosticosSeeder');
         ordenservicios::truncate();
         $this->call('OrdenServiciosSeeder');
-        //Model::reguard();
+       // Model::reguard();
 
 
     }
